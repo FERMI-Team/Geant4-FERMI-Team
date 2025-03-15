ee6f0f00eb88e43372ddaaa6bc1e45da561d6e61
FERMI-Team <terronifotonici@gmail.com>
2025-03-15 16:48:17 +0100

    physics.cc and physics.hh updated

--- sim2025/Simulation1/physics.cc
+++ sim2025/Simulation1/physics.cc
@@ -7,6 +7,14 @@ RegisterPhysics (new G4MuonMinus());
 RegisterPhysics (new G4MuonPlus());
 RegisterPhysics (new G4MuonDecayChannelWithSpin());
 RegisterPhysics (new G4MuonMinusCapture());
+
+RegisterPhysics (new G4MuonMinusBoundDecay());
+RegisterPhysics (new G4MuBetheBlochModel());
+RegisterPhysics (new G4MuBremsstrahlungModel());
+RegisterPhysics (new G4MuonicAtomDecay());
+RegisterPhysics (new G4MuonicAtomDecayPhysics());
+RegisterPhysics (new G4MuonicAtomHelper());
+RegisterPhysics (new G4MuonMinusAtomicCapture());
 }
 
 MyPhysicsList::~MyPhysicsList()
--- sim2025/Simulation1/physics.hh
+++ sim2025/Simulation1/physics.hh
@@ -8,6 +8,14 @@
 #include "G4MuonDecayChannelWithSpin.hh"
 #include "G4MuonMinusCapture.hh"
 
+#include "G4MuonMinusBoundDecay.hh"
+#include "G4MuBetheBlochModel.hh"
+#include "G4MuBremsstrahlungModel.hh"
+#include "G4MuonicAtomDecay.hh"
+#include "G4MuonicAtomDecayPhysics.hh"
+#include "G4MuonicAtomHelper.hh"
+#include "G4MuonMinusAtomicCapture.hh"
+
 
 
 class MyPhysicsList : public  G4VModularPhysicsList
